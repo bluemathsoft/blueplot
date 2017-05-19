b9eee509f647e0d8002b0aceeabae1d7ee8f7508
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-05-19 14:12:27 -0400

    add both datagroups to same plot

--- test/index.ts
+++ test/index.ts
@@ -7,18 +7,17 @@ window.onload = () => {
   let plot = new Plot();
   document.body.appendChild(plot.dom);
 
-  /*
-  let dg = new DataGroup1D(plot.width, plot.height);
-  dg.add([4,5,1,0,-80,20,43], 'bar');
-  dg.add([14,5,21,10,160,-20,-23], 'line');
-  */
+  let dg1 = new DataGroup1D(plot.width, plot.height);
+  dg1.add([4,5,1,0,-80,20,43], 'bar');
+  dg1.add([14,5,21,10,160,-20,-23], 'line');
 
-  let dg = new DataGroup2D(plot.width, plot.height);
-  dg.addPoints([
+  let dg2 = new DataGroup2D(plot.width, plot.height);
+  dg2.addPoints([
     [2,22],[4,-22],[6,-30],[8,-10]
   ],'line');
 
-  plot.add(dg);
+  // plot.add(dg1);
+  plot.add(dg2);
   plot.render();
 
   let text = new MathText('`x = (-b +- sqrt(b^2-4ac))/(2a)`')
